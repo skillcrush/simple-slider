78059e4a729aefde2d3b789c52a14e564f2a29d1
Cassandra Wilcox <cassie@codehangar.io>
2017-10-12 12:02:42 -0400

    feat: slider uses config, requirements and todos defined

--- simple-slider.js
+++ simple-slider.js
@@ -1,1 +1,39 @@
-console.log('Hello Simple Slider!', $);
+var SimpleSlider = (function ($) {
+
+  var slider = {};
+
+  // set slider config defaults
+  slider.config = {
+    showArrows: true,
+    slideDuration: 5000
+  };
+
+  // initialize slider with config
+  slider.init = function(config){
+    if(config && typeof(config) == 'object') {
+      $.extend(slider.config, config);
+    }
+    console.log(slider.config);
+  };
+
+  // REQUIREMENTS
+  // Slide Automatically
+  // Track current slide
+  // Navigate to next slide
+  // Navigate to previous slide
+  // Navigate to slide by index
+
+  // TODOS
+  // Get slider element
+  // get next button element
+  // get prev button element
+  // get dots container element
+
+
+  return slider;
+}(jQuery)); //pass in any needed global variables
+
+// initialize slider with config overrides
+SimpleSlider.init({
+  showArrows: false
+});
