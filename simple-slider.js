051f76914d27bd61a85c2b74c3aa2450314b17b4
Cassandra Wilcox <cassie@codehangar.io>
2017-10-12 22:14:14 -0400

    refactor: remove jQuery, use ES6

--- simple-slider.js
+++ simple-slider.js
@@ -1,35 +1,34 @@
-var SimpleSlider = (function ($) {
+const SimpleSlider = (function () {
 
-  var slider = {},
-    $container,
-    $slides;
+  let slider = {},
+    container,
+    slides;
 
   // set slider config defaults
   slider.config = {
-    containerSelector: '#simpleSlider',
+    containerId: 'simpleSlider',
     showArrows: true,
     slideDuration: 5000,
     auto: true
   };
 
   // initialize slider with config
-  slider.init = function (config) {
+  slider.init = config => {
     if (config && typeof(config) == 'object') {
-      $.extend(slider.config, config);
+      // $.extend(slider.config, config);
+      slider.config = Object.assign({}, slider.config, config);
     }
 
     // get slider element
-    $container = $(slider.config.containerSelector);
+    container = document.getElementById(slider.config.containerId);
     // get slides
-    $slides = $container.find('.slide');
+    slides = container.getElementsByClassName('slide');
     // get next button element
     // get prev button element
     // get dots container element
 
     // Slide Automatically or Nah...
-    if (slider.config.auto) {
-      autoNext();
-    }
+    if (slider.config.auto) autoNext();
 
   };
 
@@ -39,61 +38,45 @@ var SimpleSlider = (function ($) {
   }
 
   // Navigate to next slide
-  slider.next = function () {
-    var currentIndex;
+  slider.next = () => {
     // get all slides
     // find active slide
-    var $activeSlide = $slides.filter(function (index) {
-      if ($(this).hasClass('active')) {
-        currentIndex = index;
-      }
-      return $(this).hasClass('active');
-    });
+    const activeSlide = [...slides].filter( slide => slide.classList.contains('active') )[0];
+    const currentIndex = [...slides].indexOf(activeSlide);
     // remove active class from active slide
-    $activeSlide.removeClass('active');
+    activeSlide.classList.remove('active');
     // apply active class to next slide
     // handle when next slide is first slide
-    if ($slides.length === currentIndex + 1) {
-      $($slides[0]).addClass('active');
+    if (slides.length === currentIndex + 1) {
+      slides[0].classList.add('active');
     } else {
-      $($slides[currentIndex + 1]).addClass('active');
+      slides[currentIndex + 1].classList.add('active');
     }
   };
 
   // Navigate to previous slide
-  slider.prev = function () {
-    var currentIndex;
+  slider.prev = () => {
     // get all slides
     // find active slide
-    var $activeSlide = $slides.filter(function (index) {
-      if ($(this).hasClass('active')) {
-        currentIndex = index;
-      }
-      return $(this).hasClass('active');
-    });
+    const activeSlide = [...slides].filter( slide => slide.classList.contains('active') )[0];
+    const currentIndex = [...slides].indexOf(activeSlide);
     // remove active class from active slide
-    $activeSlide.removeClass('active');
-    // apply active class to prev slide
-    // handle when prev slide is last slide
+    activeSlide.classList.remove('active');
+    // apply active class to next slide
+    // handle when next slide is first slide
     if (currentIndex === 0) {
-      $($slides[$slides.length - 1]).addClass('active');
+      slides[slides.length - 1].classList.add('active');
     } else {
-      $($slides[currentIndex - 1]).addClass('active');
+      slides[currentIndex - 1].classList.add('active');
     }
   };
 
   // Navigate to slide by index
-  slider.setSlideByIndex = function (index) {
+  slider.setSlideByIndex = index => {
 
   };
 
   // Track current slide
 
   return slider;
-}(jQuery)); //pass in any needed global variables
-
-// initialize slider with config overrides
-SimpleSlider.init({
-  showArrows: false,
-  slideDuration: 1000
-});
+}()); //pass in any needed global variables
