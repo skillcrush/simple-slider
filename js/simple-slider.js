996e6bde09be0dbf59ca38c1f0bc7a62b504ea86
Cassandra Wilcox <cassie@codehangar.io>
2017-10-17 15:06:19 -0400

    refactor: latest JS & CSS updates from codepen project with nellaro

--- js/simple-slider.js
+++ js/simple-slider.js
@@ -1,36 +1,56 @@
 
-const SimpleSlider = (function () {
+const SimpleSlider = (function ($) {
 
   let slider = {},
-    container,
-    slides;
+    $container,
+    $slides,
+    $prev,
+    $next,
+    $dots;
 
   // set slider config defaults
   slider.config = {
-    containerId: 'simpleSlider',
-    showArrows: true,
     slideDuration: 5000,
-    auto: true
+    auto: true,
+    containerSelector: '#simpleSlider',
+    slideSelector: 'p',
+    prevArrowSelector: '.arrows.prev',
+    nextArrowSelector: '.arrows.next',
+    dotsSelector: '.dot'
   };
 
   // initialize slider with config
   slider.init = config => {
+    // if config provided, merge it with default config
     if (config && typeof(config) == 'object') {
-      // $.extend(slider.config, config);
-      slider.config = Object.assign({}, slider.config, config);
+      $.extend(slider.config, config);
     }
-
     // get slider element
-    container = document.getElementById(slider.config.containerId);
+    $container = $(slider.config.containerSelector);
     // get slides
-    slides = container.getElementsByClassName('slide');
-    // get next button element
+    $slides = $container.find(slider.config.slideSelector);
     // get prev button element
+    $prev = $(slider.config.prevArrowSelector);
+    // get next button element
+    $next = $(slider.config.nextArrowSelector);
     // get dots container element
-
+    $dots = $(slider.config.dotsSelector);
+    // hook up prev button
+    $prev.click(slider.prev);
+    // hook up next button
+    $next.click(slider.next);
+    // hook up dots nav
+    $dots.each( (i, dot) => {
+      $(dot).click( () => {
+        slider.setSlideByIndex($dots.index(dot));
+      });
+    });
+    // activate first slide
+    $($slides[0]).addClass('activeText');
+    // activate first dot
+    $($dots[0]).addClass('active');
     // Slide Automatically or Nah...
     if (slider.config.auto) autoNext();
-
   };
 
   // Slide Automatically
@@ -40,44 +60,71 @@ const SimpleSlider = (function () {
 
   // Navigate to next slide
   slider.next = () => {
-    // get all slides
-    // find active slide
-    const activeSlide = [...slides].filter( slide => slide.classList.contains('active') )[0];
-    const currentIndex = [...slides].indexOf(activeSlide);
+    // get active slide
+    const activeSlide = $slides.filter('.activeText');
+    // get active dot
+    const activeDot = $dots.filter('.active');
+    // get current index
+    const currentIndex = $slides.index(activeSlide);
     // remove active class from active slide
-    activeSlide.classList.remove('active');
-    // apply active class to next slide
-    // handle when next slide is first slide
-    if (slides.length === currentIndex + 1) {
-      slides[0].classList.add('active');
+    activeSlide.removeClass('activeText');
+    activeDot.removeClass('active');
+    // apply activeText class to next slide
+    // if on last slide
+    if (currentIndex === $slides.length -  1) {
+      // make first slide active
+      $($slides[0]).addClass('activeText');
+      // make first dot active
+      $($dots[0]).addClass('active');
     } else {
-      slides[currentIndex + 1].classList.add('active');
+      // make next slide active
+      $($slides[currentIndex + 1]).addClass('activeText');
+      // make next slide dot
+      $($dots[currentIndex + 1]).addClass('active');
     }
   };
 
   // Navigate to previous slide
   slider.prev = () => {
-    // get all slides
-    // find active slide
-    const activeSlide = [...slides].filter( slide => slide.classList.contains('active') )[0];
-    const currentIndex = [...slides].indexOf(activeSlide);
+    // get active slide
+    const activeSlide = $slides.filter('.activeText');
+    // get active dot
+    const activeDot = $dots.filter('.active');
+    // get current index
+    const currentIndex = $slides.index(activeSlide);
     // remove active class from active slide
-    activeSlide.classList.remove('active');
-    // apply active class to next slide
+    activeSlide.removeClass('activeText');
+    activeDot.removeClass('active');
+    // apply activeText class to next slide
     // handle when next slide is first slide
     if (currentIndex === 0) {
-      slides[slides.length - 1].classList.add('active');
+      // make last slide active
+      $slides[$slides.length - 1].classList.add('activeText');
+      // make last dot active
+      $dots[$dots.length - 1].classList.add('active');
     } else {
-      slides[currentIndex - 1].classList.add('active');
+      // make prev slide active
+      $($slides[currentIndex - 1]).addClass('activeText');
+      // make prev dot active
+      $($dots[currentIndex - 1]).addClass('active');
     }
   };
 
   // Navigate to slide by index
   slider.setSlideByIndex = index => {
-
+    // get active slide
+    const activeSlide = $slides.filter('.activeText');
+    // get active dot
+    const activeDot = $dots.filter('.active');
+    // remove active class from active slide & dot
+    activeSlide.removeClass('activeText');
+    activeDot.removeClass('active');
+    // make slide at given index active
+    $($slides[index]).addClass('activeText');
+    // make slide at given index active
+    $($dots[index]).addClass('active');
   };
 
-  // Track current slide
-
+  // return the slider object with public methods
   return slider;
-}()); //pass in any needed global variables
+}(jQuery)); //pass in any needed global variables
---
+++ js/step2-complete.js
@@ -0,0 +1,53 @@
+// SimpleSlider is an immediately invoked function expression (IIFE)
+// It returns an object with public methods and properties that can be used to configure and control the slider without modifying the source code
+// it obscures private finctions and variables
+const SimpleSlider = (function ($) {
+
+  // initialize "global" variables
+  let slider = {};
+
+  // set slider config defaults
+  slider.config = {
+
+  };
+
+  // initialize slider with config
+  slider.init = config => {
+    // if config provided, merge it with default config
+    // get slider element
+    // get slides
+    // get prev button element
+    // get next button element
+    // get dots container element
+    // hook up prev button
+    // hook up next button
+    // hook up dots nav
+    // activate first slide
+    // activate first dot
+    // Slide Automatically or Nah...
+  };
+
+  // Slide Automatically
+  // private function
+  function autoNext() {
+  }
+
+  // Navigate to next slide
+  // public method
+  slider.next = () => {
+
+  };
+
+  // Navigate to previous slide
+  slider.prev = () => {
+
+  };
+
+  // Navigate to slide by index
+  slider.setSlideByIndex = index => {
+
+  };
+
+  // return the slider object with public methods
+  return slider;
+}(jQuery)); //pass in any needed global variables
